de549aab9f6d9bbd5e85e8cf59ce52d2f9659591
Taynan Vieira <taynan.vieira.one@gmail.com>
2021-05-28 12:25:06 -0300

    Separando codigos de exemplo em metodos isolados

--- src/main/java/paradigmafuncional/ParadigmaFuncional.java
+++ src/main/java/paradigmafuncional/ParadigmaFuncional.java
@@ -4,7 +4,11 @@ import java.util.function.UnaryOperator;
 
 public class ParadigmaFuncional {
     public static void main(String[] args) {
-        UnaryOperator<Integer> calcularValorVezesTres = valor -> valor*3;
-        System.out.println("O resultado é:: " + calcularValorVezesTres.apply(10));
+        calcularValorVezesTres();
+    }
+
+    private static void calcularValorVezesTres() {
+        UnaryOperator<Integer> resultado = valor -> valor*3;
+        System.out.println("O resultado é:: " + resultado.apply(10));
     }
 }
--- src/main/java/paradigmafuncional/ParadigmaImperativo.java
+++ src/main/java/paradigmafuncional/ParadigmaImperativo.java
@@ -2,6 +2,10 @@ package paradigmafuncional;
 
 public class ParadigmaImperativo {
     public static void main(String[] args) {
+        calcularValorVezesTres();
+    }
+
+    private static void calcularValorVezesTres() {
         int valor = 10;
         int resultado = valor * 3;
         System.out.println("O resultado é ::" +resultado);
