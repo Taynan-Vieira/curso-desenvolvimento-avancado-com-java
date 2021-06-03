be175b586034c490bf752f08a6df1ff5b52979f7
Taynan Vieira <taynan.vieira.one@gmail.com>
2021-06-03 00:30:16 -0300

    interfaces-funcionais-aula-3-Consumer (Funcoes de alta ordem)

---
+++ src/main/java/interfacesfuncionais/aula3/Consumidores.java
@@ -0,0 +1,15 @@
+package interfacesfuncionais.aula3;
+
+import java.util.function.Consumer;
+
+public class Consumidores {
+
+    public static void main(String[] args) {
+
+        Consumer<String> imprimirUmaFrase = System.out::println;
+        imprimirUmaFrase.accept("Hello Word");
+
+//        Consumer<String> imprimirUmaFrase = frase -> System.out.println("frase");
+
+    }
+}
