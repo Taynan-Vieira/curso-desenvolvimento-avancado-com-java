4aefbede9b56819bef5c8c44eb35aa36689fdd39
Taynan Vieira <taynan.vieira.one@gmail.com>
2021-06-04 22:48:15 -0300

    interfaces-funcionais-aula-4-Funcoes

---
+++ src/main/java/function/Funcoes.java
@@ -0,0 +1,13 @@
+package function;
+
+import java.util.function.Function;
+
+public class Funcoes {
+    public static void main(String[] args){
+        Function<String, String> retornaNomeAoContratio = texto -> new StringBuilder(texto).reverse().toString();
+        System.out.println(retornaNomeAoContratio.apply("Taynan"));
+
+        Function<String, Integer> converteStringParaInteiroECalculaODobro = string -> Integer.valueOf(string) * 2;
+        System.out.println(converteStringParaInteiroECalculaODobro.apply("20"));
+    }
+}
