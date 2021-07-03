1ca60a2609c34db96ec1d32b6a36034648dd9993
Taynan Vieira <taynan.vieira.one@gmail.com>
2021-07-03 11:46:59 -0300

    novidades-dojava-10-aula-2-Aplicando os novos releases da linguagem na pratica II

--- src/main/java/novidadesdojava10/aula1/InferenciaExemplo.java
+++ src/main/java/novidadesdojava10/aula1/InferenciaExemplo.java
@@ -4,16 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.stream.Collectors;
 
 public class InferenciaExemplo {
     public static void main(String[] args) throws IOException {
-        URL url = new URL("https://www.oracle.com/br/java/");
-        URLConnection urlConnection = url.openConnection();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        printarNomeCompleto("Taynan", "Vieira");
+    }
 
+    private static void connectedAndPrintUrlJavaOracle() throws IOException {
+        var url = new URL("https://www.oracle.com/br/java/");
+        var urlConnection = url.openConnection();
+        var bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
         System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
+
+    }
+
+    public static void printarNomeCompleto(String nome, String sobrenome){
+        var nomeCompleto = String.format("%s %s", nome, sobrenome);
+        System.out.println(nomeCompleto);
     }
 
 }
